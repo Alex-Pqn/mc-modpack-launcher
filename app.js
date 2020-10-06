8149903eb9bd9bdfe9b8262a7b2d7443221e2b07
Alexandre P <@paquienalexandre0@gmail.com>
2020-10-06 22:05:35 +0200

    License & Build app options

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // declarations & imports
 
-require('electron-reload')(__dirname, {
-  electron: require(`${__dirname}/node_modules/electron`),
-});
+// require('electron-reload')(__dirname, {
+//   electron: require(`${__dirname}/node_modules/electron`),
+// });
 
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
